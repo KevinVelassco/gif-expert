13efdb5771506a4b815f0d79954b29aa053046b4
KevinVelassco <kevinvelassco@gmail.com>
2021-04-27 19:13:52 -0500

    feat: GifGrid component is refactored to obtain the images from the giphy api

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { getGifs } from '../helpers/getGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
@@ -6,19 +7,9 @@ export const GifGrid = ({ category }) => {
     const [images, setimages] = useState([]);
 
     useEffect(() => {
-        getGifs();
-    }, []);
-
-    const getGifs = async () => {
-        const url = "https://api.giphy.com/v1/gifs/search?api_key=ADWbaWzWxzmvj1RwlWZuVgLZIRSizaU3&q=one piece";
-
-        const response = await fetch(url);
-        const { data } = await response.json();
-
-        const gifs = data.map(img => ({ id: img.id, title: img.title, url: img.images?.downsized_medium.url }));
-
-        setimages(gifs);
-    }
+        getGifs(category)
+            .then(setimages)
+    }, [category]);
 
     return (
         <>
