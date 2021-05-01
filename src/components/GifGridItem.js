bb2c96aa116f25eb7df46aa550373e1f220b9ff8
KevinVelassco <kevinvelassco@gmail.com>
2021-05-01 11:34:03 -0500

    feat: is added style the GifGridItem component

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -6,9 +6,6 @@ export const GifGridItem = ({ title, url }) => {
             <div className="card__imgContainer">
                 <img src={url} alt={title} className="card__img" />
             </div>
-            <div className="card__InfoContainer">
-                <p>{title}</p>
-            </div>
         </div>
     )
 }
