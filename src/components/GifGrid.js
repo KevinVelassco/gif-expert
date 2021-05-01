b77ccd6d7b17bbe79af6654b3b8512a8dd74b33b
KevinVelassco <kevinvelassco@gmail.com>
2021-05-01 10:06:45 -0500

    feat: created hook useFetchGifs

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,19 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { getGifs } from '../helpers/getGifs';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
-    const [images, setimages] = useState([]);
-
-    useEffect(() => {
-        getGifs(category)
-            .then(setimages)
-    }, [category]);
+    const { data: images, loading } = useFetchGifs(category);
 
     return (
         <>
             <h2>{category}</h2>
+            { loading && <p>Loading</p>}
             <div className="panel">
                 {
                     images.map(img => (
